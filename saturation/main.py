2e0f4793b29edeb2159d3effe5bdb55e3e749f96
Michael Kotliar <misha.kotliar@gmail.com>
2018-06-13 11:03:20 -0400

    Export plots to png

--- saturation/main.py
+++ saturation/main.py
@@ -4,29 +4,68 @@ import sys
 import argparse
 import tempfile
 import shutil
-from saturation.utils import (normalize_args, export_to_file, get_macs_command_line, parse_macs_log)
+from saturation.utils import (normalize_args, export_to_file, get_macs_command_line, parse_macs_log, save_plot)
 
 
 def get_parser():
     parser = argparse.ArgumentParser(description='SatScript', add_help=True)
     parser.add_argument("-b", "--bam",        type=str, help="Path to the BAM file",   required=True)
     parser.add_argument("-m", "--macs",       type=str, help="Path to the MACS2 log file", required=True)
-    parser.add_argument("-f", "--file",       type=str, help="Output filename. If not absolute, resolve relative to the current directory", default="Satur.txt")
+    parser.add_argument("-o", "--output",     type=str, help="Output filename prefix", default="./")
+    parser.add_argument("-s", "--suffix",     type=str, help="Output suffixes for reads, islands, surface and saturation files", nargs=4,
+                        default=["reads.png", "islands.png", "surface.png", "saturation.txt"])
     parser.add_argument("-p", "--percentage", type=str, help="Target percentage", nargs="*", default=["25", "50", "75", "90", "95", "98", "99", "99.5", "100"])
     parser.add_argument("-t", "--temp",       type=str, help="Temp folder", default=".")
     return parser
+
+
+def export_results(args, output_data):
+
+    percent = [line[0] for line in output_data]
+    total_mapped = [line[1] for line in output_data]
+    macs2_reads = [line[2] for line in output_data]
+    islands = [line[3] for line in output_data]
+    surface = [line[4] for line in output_data]
+
+    save_plot(filename=args.output + args.suffix[0],
+              title="Reads",
+              x_data=percent,
+              y_data=[total_mapped, macs2_reads],
+              labels=["Total mapped reads", "Reads used by MACS"],
+              styles=["ro-", "bo-"],
+              axis=["%", "reads"])
+
+    save_plot(filename=args.output + args.suffix[1],
+              title="Islands",
+              x_data=percent,
+              y_data=[islands],
+              labels=["islands"],
+              styles=["bo-"],
+              axis=["%", "islands"])
+
+    save_plot(filename=args.output + args.suffix[2],
+              title="Surface",
+              x_data=percent,
+              y_data=[surface],
+              labels=["surface"],
+              styles=["bo-"],
+              axis=["%", "surface, bp"])
+
+    export_to_file(args.output + args.suffix[3], "\n".join([" ".join(map(str, line)) for line in output_data]))
 
 
 def main(argsl=None):
     if argsl is None:
         argsl = sys.argv[1:]
     args,_ = get_parser().parse_known_args(argsl)
-    args = normalize_args(args, ["percentage"])
+    args = normalize_args(args, ["percentage", "suffix", "output"])
 
+    print (args)
     macs_command_line = get_macs_command_line(args.macs)
     temp_folder = tempfile.mkdtemp(prefix=os.path.join(args.temp, "tmp_"))
     try:
         output_data = []
+
         for target_percent in args.percentage:
             rand_sample_output = os.path.join(temp_folder, target_percent + ".bam")
             call_peak_output = os.path.join(temp_folder, target_percent)
@@ -34,8 +73,9 @@ def main(argsl=None):
             os.system(" ".join(["macs2", macs_command_line, "-t", rand_sample_output, "-n", call_peak_output]))
             result = parse_macs_log(call_peak_output + "_peaks.xls", target_percent)
             output_data.append(result)
-        output_data = "\n".join([" ".join(map(str, line)) for line in output_data])
-        export_to_file(args.file, output_data)
+
+        export_results(args, output_data)
+
     except Exception as err:
         print("Error", err)
         raise
--- saturation/utils.py
+++ saturation/utils.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import os
 import argparse
+import matplotlib.pyplot as plt
 
 
 def open_file(filename):
@@ -32,12 +33,12 @@ def get_macs_command_line(macs_log, exclude_args=["-t", "-n", "-f"]):
 
 def parse_macs_log(xlsfile, target_percent):
     ttags, ftags, islands_n, islands_len, istart = 0, 0, 0, 0, 0
-    result = [target_percent]
+    result = [float(target_percent)]
     for line in open_file(xlsfile):
         if "# total tags in treatment" in line:
-            ttags = line.split(":")[1].strip()
+            ttags = int(line.split(":")[1].strip())
         elif "# tags after filtering in treatment" in line:
-            ftags = line.split(":")[1].strip()
+            ftags = int(line.split(":")[1].strip())
         elif ("#" not in line) and ("start" not in line) and (istart != int(line.strip().split()[1])):
             islands_n = islands_n + 1
             istart = int(line.strip().split()[1])
@@ -54,3 +55,27 @@ def normalize_args(args, skip_list=[]):
         else:
             normalized_args[key]=value
     return argparse.Namespace (**normalized_args)
+
+
+def save_plot(filename, x_data, y_data, styles, labels, axis, title="", padding=[5, 5]):
+    x_max = max(x_data)
+    y_max = max([max(y_data_line) for y_data_line in y_data])
+    x_pad = 0.01 * padding[0] * x_max
+    y_pad = 0.01 * padding[1] * y_max
+
+    handles = []
+    for data_combined in zip(y_data, styles, labels):
+        current_handle, = plt.plot(x_data, data_combined[0], data_combined[1], label=data_combined[2], mfc='none')
+        handles.append(current_handle)
+
+    plt.title(title)
+    plt.xlabel(axis[0])
+    plt.ylabel(axis[1])
+    plt.legend(handles=handles)
+    plt.axis([0 - x_pad, x_max + x_pad, 0 - y_pad, y_max + y_pad])
+    plt.ticklabel_format(style='sci', axis='y', scilimits=(0, 0))
+    plt.savefig(filename, bbox_inches='tight', dpi=150)
+    plt.close('all')
+
+
+
